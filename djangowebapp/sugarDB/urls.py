6eab48ec0006ee055d57d75c4cff8e0233d7a955
Zoe-2001 <yuchenda@andrew.cmu.edu>
2024-03-21 04:48:56 -0400

    integrated with the frontend design

--- djangowebapp/sugarDB/urls.py
+++ djangowebapp/sugarDB/urls.py
@@ -3,10 +3,13 @@ from django.urls import path, include
 from sugarDB import views
 
 urlpatterns = [
-    path('', views.main_action),
-    path('upload-gml', views.file_upload, name='upload-gml'),
-    path('main', views.main_action, name='main'),
-    path('api/microgrid-data/', views.microgrid_data_view, name='microgrid-data'),
+    path('', views.visualization_action),
+    path('forecasting', views.forecasting_action, name='forecasting'),
+    path('upload', views.upload_action, name='upload'),
+    path('visualization', views.visualization_action, name='visualization'),
+    #path('upload-gml', views.file_upload, name='upload-gml'),
+    #path('main', views.main_action, name='main'),
+    #path('api/microgrid-data/', views.microgrid_data_view, name='microgrid-data'),
 ]
 
 
