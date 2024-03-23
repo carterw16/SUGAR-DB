bee605bbb64cb8eb2d4b48c2b0b4ad1376c61030
Zoe-2001 <yuchenda@andrew.cmu.edu>
2024-03-23 03:56:05 -0400

    integrated with parser

--- djangowebapp/sugarDB/views.py
+++ djangowebapp/sugarDB/views.py
@@ -6,6 +6,53 @@ from .forms import GLMFileForm
 import networkx as nx
 from django.http import JsonResponse
 
+'''for parser'''
+from __future__ import division, print_function
+import os
+import sys
+sys.path.append(os.getcwd() + "/classes")
+sys.path.append(os.getcwd() + "/lib")
+sys.path.append(os.getcwd())
+from SUGAR-DB.lib.parser import parser
+import pyximport
+pyximport.install(language_level=3)
+# Create settings for parser
+infeas_settings_dict = {}
+infeas_settings_dict['source type'] = 'current' # Initial values sent in lib/intialize.py
+infeas_settings_dict['obj'] = 'L2'
+infeas_settings_dict['obj type'] = 'power'
+infeas_settings_dict['obj scalar'] = 1e-6
+
+# VOLTAGE UNBALANCE SETTINGS
+voltage_unbalance_settings_dict = {}
+# REQUIRED - either True or False
+voltage_unbalance_settings_dict['enforce voltage unbalance'] = False
+
+# VOLTAGE BOUND SETTINGS
+voltage_bound_settings_dict = {}
+# REQUIRED - either True or False
+voltage_bound_settings_dict['enforce voltage bounds'] = False
+
+# WARM START SETTINGS
+# If performing a warm start to a test, use the dictionary to pass settings
+warm_start_settings_dict = {}
+# REQUIRED - either True or False
+warm_start_settings_dict['initialize'] = False
+
+# feature: battery settings
+infeas_settings_dict['battery_node_list'] = [
+                                        {"ID": "B1", "node":"l4", "P_max":300000, "P_min":0,
+                                         "Mch": 0.00000005, "Md": 0.00000009, "type":"P", "Bt_prev":0.1, "C_ch":1, "C_d":-0.5, "single_phase":"A"}
+]
+
+SETTINGS = {
+    'infeas settings': infeas_settings_dict,
+    'Stamp Dual': True,
+    'voltage unbalance settings': voltage_unbalance_settings_dict,
+    'Warm Start': warm_start_settings_dict,
+    'voltage bound settings': voltage_bound_settings_dict,
+        }
+
 def forecasting_action(request):
     if request.method == 'GET':
         return render(request, 'sugarDB/forecasting.html')
@@ -32,12 +79,12 @@ def upload_action(request):
     # Validates the form.f
     if form.is_valid():
         # Process the file
-        #uploaded_file = request.FILES['file']  # Make sure 'file' matches the name attribute in your HTML form
-        #parsed_data = parse_gml_file(uploaded_file)
-
+        uploaded_file = request.FILES['file']  # Make sure 'file' matches the name attribute in your HTML form
+        casedata, node_key, node_index_ = parser(uploaded_file, SETTINGS)
+        print(casedata.node)
         # Redirect to visualization with parsed data
         # This approach uses session to pass data to the next request, adjust as per your requirement
-        #request.session['parsed_data'] = parsed_data
+        request.session['parsed_data'] = casedata.node
         return redirect('visualization')
     else:
         context['form'] = form
